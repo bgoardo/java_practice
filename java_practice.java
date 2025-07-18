f70d1b922c73ef0c53cafef122e2d47a8626f5d4
bgoardo <brandonmgoard@gmail.com>
2025-07-18 18:33:26 -0400

    different practice set, from geeks for geeks

--- java_practice.java
+++ java_practice.java
@@ -1,26 +1,16 @@
+import java.io.*;
 
 public class java_practice
 {
 public static void main(String[] args)
 {
-    if (args.length > 0)
-    {
-        if (args[0].equals("sleepIn"))
-        {
-            
-        }
-    }
+    java_practice ob = new java_practice();
+   
 }
-//The parameter weekday is true if it is a weekday,
-// and the parameter vacation is true if we are on vacation. 
-//We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-public boolean sleepIn(boolean weekday, boolean vacation) {
-    if ((weekday == false)||(vacation == true))
-    {
-        return true;
-    }
-    else
-    return false;
-  
+public static int sum (int a, int b)
+{
+    return a + b;
 }
+
 }
+
